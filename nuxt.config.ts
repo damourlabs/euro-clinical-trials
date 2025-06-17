c5bdebb712977082fa38eb7b0ea369ed857b3afd
Christopher <129872252+damourChris@users.noreply.github.com>
2025-06-17 21:40:08 +0200

    chore: extend from github layer instead of pacakge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['nuxt-auth-utils', '@pinia/nuxt'],
-  extends: ['@damourlabs-portfolio/ui'],
+  extends: ['github:damourlabs/ui'],
   runtimeConfig: {
     session: {
       name: 'session',
