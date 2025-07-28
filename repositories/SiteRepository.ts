01a73caac8d8cf0b5c8c664a767f9defa6c1cae3
Christopher <129872252+damourChris@users.noreply.github.com>
2025-07-28 23:05:57 +0200

    fix: update import paths for Site type in repository

--- repositories/SiteRepository.ts
+++ repositories/SiteRepository.ts
@@ -1,6 +1,6 @@
 // ~/repositories/TrialsRepository.ts
 import { ResourceRepository, type REPOSITORY_TYPE } from './ResourceRepository'
-import type { Site } from '~/models/admin'
+import type { Site } from '~/server/database/schema'
 
 export class SiteRepository extends ResourceRepository<Site> {
     constructor(type: REPOSITORY_TYPE = 'cached') {
